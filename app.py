29b5ed42c0783273416341fabed1f9f636ebd656
etayass1997 <etayass18@gmail.com>
2026-06-04 14:06:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def call_gemini(messages, system):
 
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel(
-        model_name='gemini-2.0-flash',
+        model_name='gemini-1.5-flash-latest',
         system_instruction=system
     )
 
